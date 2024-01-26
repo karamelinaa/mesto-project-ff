d3e811147354d51f071461f9b96b8afb44fdcf28
Anna Karelina <Karamelinaa94@yandex.ru>
2024-01-26 14:19:29 +0300

    первая фиксация

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
-// @todo: Темплейт карточки
+const content = document.querySelector(".content");
+const cardPlace = content.querySelector(".places__list");
+const addButton = content.querySelector(".profile__add-button");
+const cardTemplate = document.querySelector("#card-template");
 
-// @todo: DOM узлы
+let cardIndex = 0;
 
-// @todo: Функция создания карточки
+function createCard(linkValue, nameValue) {
+  const cardElement = cardTemplate.content
+    .querySelector(".places__item")
+    .cloneNode(true);
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector(".card__image").src = linkValue;
+  cardElement.querySelector(".card__image").alt = nameValue;
+  cardElement.querySelector(".card__title").textContent = nameValue;
 
-// @todo: Вывести карточки на страницу
+  cardPlace.prepend(cardElement);
+
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => {
+      deleteCard(cardElement);
+    });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+  if (cardPlace.children.length === 0) {
+    cardIndex = 0;
+  }
+}
+
+addButton.addEventListener("click", function () {
+  if (cardIndex < initialCards.length) {
+    createCard(initialCards[cardIndex].link, initialCards[cardIndex].name);
+    cardIndex++;
+  }
+});
