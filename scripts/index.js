af7f2e7c4cd037ee4230b7ed00a216a93a4ccfbb
Anna Karelina <Karamelinaa94@yandex.ru>
2024-01-29 12:18:56 +0300

    fix bug

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,21 @@
-const content = document.querySelector(".content");
-const cardPlace = content.querySelector(".places__list");
-const addButton = content.querySelector(".profile__add-button");
+// @todo: Темплейт карточки
+
 const cardTemplate = document.querySelector("#card-template");
 
-let cardIndex = 0;
+// @todo: DOM узлы
+const content = document.querySelector(".content");
+const cardPlace = content.querySelector(".places__list");
 
-function createCard(linkValue, nameValue) {
+// @todo: Функция создания карточки
+
+function createCard(card, deleteCard) {
   const cardElement = cardTemplate.content
     .querySelector(".places__item")
     .cloneNode(true);
 
-  cardElement.querySelector(".card__image").src = linkValue;
-  cardElement.querySelector(".card__image").alt = nameValue;
-  cardElement.querySelector(".card__title").textContent = nameValue;
-
-  cardPlace.prepend(cardElement);
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
+  cardElement.querySelector(".card__title").textContent = card.name;
 
   cardElement
     .querySelector(".card__delete-button")
@@ -25,20 +26,19 @@ function createCard(linkValue, nameValue) {
   return cardElement;
 }
 
-initialCards.forEach((card) => {
-    createCard(card.link, card.name);
-});
+// @todo: Функция удаления карточки
 
 function deleteCard(cardElement) {
   cardElement.remove();
-  if (cardPlace.children.length === 0) {
-    cardIndex = 0;
-  }
 }
 
-addButton.addEventListener("click", function () {
-  if (cardIndex < initialCards.length) {
-    createCard(initialCards[cardIndex].link, initialCards[cardIndex].name);
-    cardIndex++;
-  }
+// @todo: Добавление карточeк на страницу
+
+function renderCard(card) {
+  cardPlace.append(createCard(card, deleteCard));
+}
+
+// @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  renderCard(card);
 });
